e18a6c58c1936d24b282968de925a42ec8bac908
Dmitry Osipenko <digetx@gmail.com>
2019-09-29 20:59:48 +0300

    cpuidle: Introduce unified driver for NVIDIA Tegra SoCs
    The new driver is based on the old CPU Idle drivers that are removed now
    from arm/arch/mach-tegra/ directory. Those removed drivers were reworked
    and squashed into a single unified driver that covers multiple hardware
    generations, starting from Tegra20 and ending with Tegra124. The new
    driver takes slightly different approach in regards to handling of CC6
    state by parking secondary CPUs explicitly into offline state, in contrast
    to CPUs suspend/resume racing that old Tegra20 had. The new driver doesn't
    ignore any possible errors and provides useful diagnostics information in
    a case of failure.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- include/soc/tegra/cpuidle.h
+++ include/soc/tegra/cpuidle.h
@@ -6,8 +6,12 @@
 #ifndef __SOC_TEGRA_CPUIDLE_H__
 #define __SOC_TEGRA_CPUIDLE_H__
 
+#ifdef CONFIG_ARM_TEGRA_CPUIDLE
+void tegra_cpuidle_pcie_irqs_in_use(void);
+#else
 static inline void tegra_cpuidle_pcie_irqs_in_use(void)
 {
 }
+#endif
 
 #endif /* __SOC_TEGRA_CPUIDLE_H__ */
