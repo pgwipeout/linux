9f19fd983d78753a03c81a5d24cd3c543380d758
Dmitry Osipenko <digetx@gmail.com>
2019-09-29 20:59:49 +0300

    cpuidle: tegra: Support CPU cluster power-down state on Tegra30
    The new Tegra CPU Idle driver now has a unified code path for the coupled
    LP2/CC6 state, this allows to enable the deepest idling state on Tegra30
    SoC where the whole CPU cluster is power-gated.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/cpuidle/cpuidle-tegra.c
+++ drivers/cpuidle/cpuidle-tegra.c
@@ -325,7 +325,6 @@ static int tegra_cpuidle_probe(struct platform_device *pdev)
 		tegra_idle_driver.states[TEGRA_C7].disabled = true;
 		break;
 	case TEGRA30:
-		tegra_idle_driver.states[TEGRA_CC6].disabled = true;
 		break;
 	case TEGRA114:
 	case TEGRA124:
