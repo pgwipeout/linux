f2566f0b93386fb4f17c40cabc9756b18dacabd2
Dmitry Osipenko <digetx@gmail.com>
2019-11-18 00:16:45 +0300

    memory: tegra30-emc: Register as interconnect provider
    EMC now provides MC with memory bandwidth using interconnect API.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- drivers/memory/tegra/tegra30-emc.c
+++ drivers/memory/tegra/tegra30-emc.c
@@ -1367,6 +1367,10 @@ static int tegra_emc_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, emc);
 	tegra_emc_debugfs_init(emc);
 
+	err = tegra_icc_emc_setup_interconnect(&pdev->dev, 32 / 8);
+	if (err)
+		dev_err(&pdev->dev, "failed to initialize ICC: %d\n", err);
+
 	return 0;
 
 unset_cb:
