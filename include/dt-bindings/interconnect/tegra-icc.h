fb7eef9024277286ee729527da243cfdb299ecb3
Dmitry Osipenko <digetx@gmail.com>
2019-11-17 23:12:17 +0300

    dt-bindings: interconnect: tegra: Add initial IDs
    Define interconnect IDs for memory controller (MC), external memory
    controller (EMC), external memory (EMEM) and memory clients of display
    controllers (DC).
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

---
+++ include/dt-bindings/interconnect/tegra-icc.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef DT_BINDINGS_ICC_TEGRA_H
+#define DT_BINDINGS_ICC_TEGRA_H
+
+#define TEGRA_ICC_EMC			0
+#define TEGRA_ICC_EMEM			1
+#define TEGRA_ICC_MC			2
+#define TEGRA_ICC_MC_DC			3
+#define TEGRA_ICC_MC_DCB		4
+
+#endif
