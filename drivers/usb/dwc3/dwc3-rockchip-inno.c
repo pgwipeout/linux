f973a760014b531e0fd603344cfa006df318b9a3
Peter Geis <pgwipeout@gmail.com>
2020-11-16 13:30:03 +0000

    convert of-simple stuff to rk-inno

--- drivers/usb/dwc3/dwc3-rockchip-inno.c
+++ drivers/usb/dwc3/dwc3-rockchip-inno.c
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * dwc3-of-simple.c - OF glue layer for simple integrations
+ * dwc3-rockchip-inno.c - DWC3 glue layer for Rockchip devices with Innosilicon based PHY
  *
- * Copyright (c) 2015 Texas Instruments Incorporated - https://www.ti.com
- *
- * Author: Felipe Balbi <balbi@ti.com>
- *
- * This is a combination of the old dwc3-qcom.c by Ivan T. Ivanov
- * <iivanov@mm-sol.com> and the original patch adding support for Xilinx' SoC
- * by Subbaraya Sundeep Bhatta <subbaraya.sundeep.bhatta@xilinx.com>
+ * Based on dwc3-of-simple.c
  */
 
 #include <linux/module.h>
@@ -32,7 +26,7 @@
 #include "../host/xhci.h"
 
 
-struct dwc3_of_simple {
+struct dwc3_rk_inno {
 	struct device		*dev;
 	struct clk_bulk_data	*clks;
 	struct dwc3		*dwc;
@@ -46,36 +40,36 @@ struct dwc3_of_simple {
 	bool			need_host_reset;
 };
 
-static int dwc3_of_simple_host_reset_notifier(struct notifier_block *nb, unsigned long event, void *data)
+static int dwc3_rk_inno_host_reset_notifier(struct notifier_block *nb, unsigned long event, void *data)
 {
-	struct dwc3_of_simple	*simple = container_of(nb, struct dwc3_of_simple, reset_nb);
+	struct dwc3_rk_inno	*rk_inno = container_of(nb, struct dwc3_rk_inno, reset_nb);
 
-	schedule_work(&simple->reset_work);
+	schedule_work(&rk_inno->reset_work);
 
 	return NOTIFY_DONE;
 }
 
-static void dwc3_of_simple_host_reset_work(struct work_struct *work)
+static void dwc3_rk_inno_host_reset_work(struct work_struct *work)
 {
-	struct dwc3_of_simple	*simple = container_of(work, struct dwc3_of_simple, reset_work);
-	struct usb_hcd		*hcd = dev_get_drvdata(&simple->dwc->xhci->dev);
+	struct dwc3_rk_inno	*rk_inno = container_of(work, struct dwc3_rk_inno, reset_work);
+	struct usb_hcd		*hcd = dev_get_drvdata(&rk_inno->dwc->xhci->dev);
 	struct usb_hcd		*shared_hcd = hcd->shared_hcd;
 	struct xhci_hcd		*xhci = hcd_to_xhci(hcd);
 	unsigned int		count = 0;
 
-	mutex_lock(&simple->lock);
+	mutex_lock(&rk_inno->lock);
 
 	if (hcd->state != HC_STATE_HALT) {
 		usb_remove_hcd(shared_hcd);
 		usb_remove_hcd(hcd);
 	}
 
-	if (simple->phy)
-		usb_phy_shutdown(simple->phy);
+	if (rk_inno->phy)
+		usb_phy_shutdown(rk_inno->phy);
 
 	while (hcd->state != HC_STATE_HALT) {
 		if (++count > 1000) {
-			dev_err(simple->dev, "wait for HCD remove 1s timeout!\n");
+			dev_err(rk_inno->dev, "wait for HCD remove 1s timeout!\n");
 			break;
 		}
 		usleep_range(1000, 1100);
@@ -87,56 +81,56 @@ static void dwc3_of_simple_host_reset_work(struct work_struct *work)
 		usb_add_hcd(shared_hcd, hcd->irq, IRQF_SHARED);
 	}
 
-	if (simple->phy)
-		usb_phy_init(simple->phy);
+	if (rk_inno->phy)
+		usb_phy_init(rk_inno->phy);
 
-	mutex_unlock(&simple->lock);
-	dev_dbg(simple->dev, "host reset complete\n");
+	mutex_unlock(&rk_inno->lock);
+	dev_dbg(rk_inno->dev, "host reset complete\n");
 }
 
-static int dwc3_of_simple_probe(struct platform_device *pdev)
+static int dwc3_rk_inno_probe(struct platform_device *pdev)
 {
-	struct dwc3_of_simple	*simple;
+	struct dwc3_rk_inno	*rk_inno;
 	struct device		*dev = &pdev->dev;
 	struct device_node	*np = dev->of_node, *child, *node;
 	struct platform_device	*child_pdev;
 
 	int			ret;
 
-	simple = devm_kzalloc(dev, sizeof(*simple), GFP_KERNEL);
-	if (!simple)
+	rk_inno = devm_kzalloc(dev, sizeof(*rk_inno), GFP_KERNEL);
+	if (!rk_inno)
 		return -ENOMEM;
 
-	platform_set_drvdata(pdev, simple);
-	simple->dev = dev;
+	platform_set_drvdata(pdev, rk_inno);
+	rk_inno->dev = dev;
 
 	/*
 	 * Some controllers need to toggle the usb3-otg reset before trying to
 	 * initialize the PHY, otherwise the PHY times out.
 	 */
 	if (of_device_is_compatible(np, "rockchip,rk3328-dwc3")) {
-		simple->need_reset = true;
-		simple->need_host_reset = true;
+		rk_inno->need_reset = true;
+		rk_inno->need_host_reset = true;
 	}
 
-	simple->resets = of_reset_control_array_get(np, false, true,
+	rk_inno->resets = of_reset_control_array_get(np, false, true,
 						    true);
-	if (IS_ERR(simple->resets)) {
-		ret = PTR_ERR(simple->resets);
+	if (IS_ERR(rk_inno->resets)) {
+		ret = PTR_ERR(rk_inno->resets);
 		dev_err(dev, "failed to get device resets, err=%d\n", ret);
 		return ret;
 	}
 
-	ret = reset_control_deassert(simple->resets);
+	ret = reset_control_deassert(rk_inno->resets);
 	if (ret)
 		goto err_resetc_put;
 
-	ret = clk_bulk_get_all(simple->dev, &simple->clks);
+	ret = clk_bulk_get_all(rk_inno->dev, &rk_inno->clks);
 	if (ret < 0)
 		goto err_resetc_assert;
 
-	simple->num_clocks = ret;
-	ret = clk_bulk_prepare_enable(simple->num_clocks, simple->clks);
+	rk_inno->num_clocks = ret;
+	ret = clk_bulk_prepare_enable(rk_inno->num_clocks, rk_inno->clks);
 	if (ret)
 		goto err_resetc_assert;
 
@@ -144,7 +138,7 @@ static int dwc3_of_simple_probe(struct platform_device *pdev)
 	if (ret)
 		goto err_clk_put;
 
-	if (simple->need_host_reset) {
+	if (rk_inno->need_host_reset) {
 		child = of_get_child_by_name(np, "dwc3");
 		if (!child) {
 			dev_err(dev, "failed to find dwc3 core node\n");
@@ -159,18 +153,18 @@ static int dwc3_of_simple_probe(struct platform_device *pdev)
 			goto err_plat_depopulate;
 		}
 
-		simple->dwc = platform_get_drvdata(child_pdev);
-		if (!simple->dwc || !simple->dwc->xhci) {
+		rk_inno->dwc = platform_get_drvdata(child_pdev);
+		if (!rk_inno->dwc || !rk_inno->dwc->xhci) {
 			ret = -EPROBE_DEFER;
 			goto err_plat_depopulate;
 		}
 
 		node = of_parse_phandle(child, "usb-phy", 0);
-		INIT_WORK(&simple->reset_work, dwc3_of_simple_host_reset_work);
-		simple->reset_nb.notifier_call = dwc3_of_simple_host_reset_notifier;
-		simple->phy = devm_usb_get_phy_by_node(dev, node, &simple->reset_nb);
+		INIT_WORK(&rk_inno->reset_work, dwc3_rk_inno_host_reset_work);
+		rk_inno->reset_nb.notifier_call = dwc3_rk_inno_host_reset_notifier;
+		rk_inno->phy = devm_usb_get_phy_by_node(dev, node, &rk_inno->reset_nb);
 		of_node_put(node);
-		mutex_init(&simple->lock);
+		mutex_init(&rk_inno->lock);
 	}
 
 	pm_runtime_set_active(dev);
@@ -183,110 +177,110 @@ err_plat_depopulate:
 	of_platform_depopulate(dev);
 
 err_clk_put:
-	clk_bulk_disable_unprepare(simple->num_clocks, simple->clks);
-	clk_bulk_put_all(simple->num_clocks, simple->clks);
+	clk_bulk_disable_unprepare(rk_inno->num_clocks, rk_inno->clks);
+	clk_bulk_put_all(rk_inno->num_clocks, rk_inno->clks);
 
 err_resetc_assert:
-	reset_control_assert(simple->resets);
+	reset_control_assert(rk_inno->resets);
 
 err_resetc_put:
-	reset_control_put(simple->resets);
+	reset_control_put(rk_inno->resets);
 	return ret;
 }
 
-static void __dwc3_of_simple_teardown(struct dwc3_of_simple *simple)
+static void __dwc3_rk_inno_teardown(struct dwc3_rk_inno *rk_inno)
 {
-	of_platform_depopulate(simple->dev);
+	of_platform_depopulate(rk_inno->dev);
 
-	clk_bulk_disable_unprepare(simple->num_clocks, simple->clks);
-	clk_bulk_put_all(simple->num_clocks, simple->clks);
-	simple->num_clocks = 0;
+	clk_bulk_disable_unprepare(rk_inno->num_clocks, rk_inno->clks);
+	clk_bulk_put_all(rk_inno->num_clocks, rk_inno->clks);
+	rk_inno->num_clocks = 0;
 
-	reset_control_assert(simple->resets);
+	reset_control_assert(rk_inno->resets);
 
-	reset_control_put(simple->resets);
+	reset_control_put(rk_inno->resets);
 
-	pm_runtime_disable(simple->dev);
-	pm_runtime_put_noidle(simple->dev);
-	pm_runtime_set_suspended(simple->dev);
+	pm_runtime_disable(rk_inno->dev);
+	pm_runtime_put_noidle(rk_inno->dev);
+	pm_runtime_set_suspended(rk_inno->dev);
 }
 
-static int dwc3_of_simple_remove(struct platform_device *pdev)
+static int dwc3_rk_inno_remove(struct platform_device *pdev)
 {
-	struct dwc3_of_simple	*simple = platform_get_drvdata(pdev);
+	struct dwc3_rk_inno	*rk_inno = platform_get_drvdata(pdev);
 
-	__dwc3_of_simple_teardown(simple);
+	__dwc3_rk_inno_teardown(rk_inno);
 
 	return 0;
 }
 
-static void dwc3_of_simple_shutdown(struct platform_device *pdev)
+static void dwc3_rk_inno_shutdown(struct platform_device *pdev)
 {
-	struct dwc3_of_simple	*simple = platform_get_drvdata(pdev);
+	struct dwc3_rk_inno	*rk_inno = platform_get_drvdata(pdev);
 
-	__dwc3_of_simple_teardown(simple);
+	__dwc3_rk_inno_teardown(rk_inno);
 }
 
-static int __maybe_unused dwc3_of_simple_runtime_suspend(struct device *dev)
+static int __maybe_unused dwc3_rk_inno_runtime_suspend(struct device *dev)
 {
-	struct dwc3_of_simple	*simple = dev_get_drvdata(dev);
+	struct dwc3_rk_inno	*rk_inno = dev_get_drvdata(dev);
 
-	clk_bulk_disable(simple->num_clocks, simple->clks);
+	clk_bulk_disable(rk_inno->num_clocks, rk_inno->clks);
 
 	return 0;
 }
 
-static int __maybe_unused dwc3_of_simple_runtime_resume(struct device *dev)
+static int __maybe_unused dwc3_rk_inno_runtime_resume(struct device *dev)
 {
-	struct dwc3_of_simple	*simple = dev_get_drvdata(dev);
+	struct dwc3_rk_inno	*rk_inno = dev_get_drvdata(dev);
 
-	return clk_bulk_enable(simple->num_clocks, simple->clks);
+	return clk_bulk_enable(rk_inno->num_clocks, rk_inno->clks);
 }
 
-static int __maybe_unused dwc3_of_simple_suspend(struct device *dev)
+static int __maybe_unused dwc3_rk_inno_suspend(struct device *dev)
 {
-	struct dwc3_of_simple *simple = dev_get_drvdata(dev);
+	struct dwc3_rk_inno *rk_inno = dev_get_drvdata(dev);
 
-	if (simple->need_reset)
-		reset_control_assert(simple->resets);
+	if (rk_inno->need_reset)
+		reset_control_assert(rk_inno->resets);
 
 	return 0;
 }
 
-static int __maybe_unused dwc3_of_simple_resume(struct device *dev)
+static int __maybe_unused dwc3_rk_inno_resume(struct device *dev)
 {
-	struct dwc3_of_simple *simple = dev_get_drvdata(dev);
+	struct dwc3_rk_inno *rk_inno = dev_get_drvdata(dev);
 
-	if (simple->need_reset)
-		reset_control_deassert(simple->resets);
+	if (rk_inno->need_reset)
+		reset_control_deassert(rk_inno->resets);
 
 	return 0;
 }
 
-static const struct dev_pm_ops dwc3_of_simple_dev_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(dwc3_of_simple_suspend, dwc3_of_simple_resume)
-	SET_RUNTIME_PM_OPS(dwc3_of_simple_runtime_suspend,
-			dwc3_of_simple_runtime_resume, NULL)
+static const struct dev_pm_ops dwc3_rk_inno_dev_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(dwc3_rk_inno_suspend, dwc3_rk_inno_resume)
+	SET_RUNTIME_PM_OPS(dwc3_rk_inno_runtime_suspend,
+			dwc3_rk_inno_runtime_resume, NULL)
 };
 
-static const struct of_device_id of_dwc3_simple_match[] = {
+static const struct of_device_id of_dwc3_rk_inno_match[] = {
 	{ .compatible = "rockchip,rk3328-dwc3" },
 	{ /* Sentinel */ }
 };
-MODULE_DEVICE_TABLE(of, of_dwc3_simple_match);
+MODULE_DEVICE_TABLE(of, of_dwc3_rk_inno_match);
 
-static struct platform_driver dwc3_of_simple_driver = {
-	.probe		= dwc3_of_simple_probe,
-	.remove		= dwc3_of_simple_remove,
-	.shutdown	= dwc3_of_simple_shutdown,
+static struct platform_driver dwc3_rk_inno_driver = {
+	.probe		= dwc3_rk_inno_probe,
+	.remove		= dwc3_rk_inno_remove,
+	.shutdown	= dwc3_rk_inno_shutdown,
 	.driver		= {
-		.name	= "dwc3-of-simple",
-		.of_match_table = of_dwc3_simple_match,
-		.pm	= &dwc3_of_simple_dev_pm_ops,
+		.name	= "dwc3-rk-inno",
+		.of_match_table = of_dwc3_rk_inno_match,
+		.pm	= &dwc3_rk_inno_dev_pm_ops,
 	},
 };
 
-module_platform_driver(dwc3_of_simple_driver);
+module_platform_driver(dwc3_rk_inno_driver);
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("DesignWare USB3 OF Simple Glue Layer");
-MODULE_AUTHOR("Felipe Balbi <balbi@ti.com>");
+MODULE_DESCRIPTION("DesignWare USB3 Rockchip Innosilicon Glue Layer");
+MODULE_AUTHOR("Peter Geis <pgwipeout@gmail.com>");
